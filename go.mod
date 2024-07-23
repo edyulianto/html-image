e4fa7709d730d5d63847dba43c28ab97f1c77512
Edi Yulianto <edi.yulianto@biofarma.co.id>
2024-07-23 17:01:56 +0700

    feat: update code for generarte id card

--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module generate-pdf
 
 go 1.20
 
-require github.com/SebastiaanKlippert/go-wkhtmltopdf v1.9.2
+require (
+	github.com/SebastiaanKlippert/go-wkhtmltopdf v1.9.2
+	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646
+	golang.org/x/image v0.18.0
+)
+
+require github.com/h2non/bimg v1.1.9 // indirect
--- main.go
+++ main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"image"
+	"image/jpeg"
+	_ "image/png"
 	"log"
+	"os"
 	"os/exec"
+
+	"github.com/nfnt/resize"
 )
 
 func main() {
 	// Define the command to execute wkhtmltoimage
-	cmd := exec.Command("wkhtmltoimage", "--height", "860", "--width", "540", "--disable-smart-width", "--quality", "100", "id-card-design.html", "output.png")
+	cmd := exec.Command("wkhtmltoimage", "--disable-smart-width", "--quality", "100", "--zoom", "1", "id-card-design.html", "output.png")
 
 	// Run the command and capture any errors
 	err := cmd.Run()
@@ -16,4 +22,28 @@ func main() {
 	}
 
 	log.Println("Image generated successfully")
+
+	file, err := os.Open("output.png")
+	if err != nil {
+		log.Fatalf("Failed to open file: %v", err)
+	}
+	defer file.Close()
+
+	image, _, err := image.Decode(file)
+	if err != nil {
+		log.Fatalf("Failed to encode image: %v", err)
+	}
+
+	newImage := resize.Resize(227, 0, image, resize.NearestNeighbor)
+
+	// Encode uses a Writer, use a Buffer if you need the raw []byte
+	outFile, err := os.Create("resized.jpg")
+	if err != nil {
+		log.Fatalf("Failed to create file: %v", err)
+	}
+	defer outFile.Close()
+	err = jpeg.Encode(outFile, newImage, nil)
+	if err != nil {
+		log.Fatalf("Failed to encode image: %v", err)
+	}
 }
